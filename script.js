73e38d4b66f8ada338f92af0a2fc55af568b5b46
Shahed-BD71 <mdshahedbd71@gmail.com>
2021-01-31 14:50:16 +0600

    fix

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-function handleTicketChange(ticket, isIncrease){
-const ticketCount = getInputValue(ticket);
-let ticketNewCount = ticketCount;
-if (isIncrease == true) {
-    ticketNewCount = ticketCount + 1;
-}
-if (isIncrease == false && ticketCount > 0) {
-    ticketNewCount = ticketCount - 1;
-}
-document.getElementById(ticket + '-count').innerText = ticketNewCount;
-let ticketTotal = 0;
-if (ticket == 'firstClass') {
-    ticketTotal = ticketNewCount * 150;
-}
-if (ticket == 'economy') {
-    ticketTotal = ticketNewCount * 100;
-}
-calculateTotal();
-document.getElementById(ticket + '-total').innerText = ticketTotal;
-}
+function handleTicketChange (ticket, isIncrease){
+    const ticketInput = document.getElementById(ticket  + '-count');
+    const ticketCount = parseInt(ticketInput.value);
+    let ticketNewCount = ticketCount;
+    if (isIncrease == true) {
+        ticketNewCount = ticketCount + 1;
+    }
+    if (isIncrease == false && ticketCount > 0) {
+        ticketNewCount = ticketCount - 1;
+    }
+    document.getElementById(ticket + '-count').value = ticketNewCount;
+    // firstClassTotal = firstClassNewCount * 150;
+    let ticketTotal = 0;
+    if (ticket == 'firstClass') {
+        ticketTotal = ticketNewCount * 150;
+    }
+    if (ticket == 'economy') {
+        ticketTotal = ticketNewCount * 100;
+    }
+    document.getElementById(ticket + "-total").innerText = ticketTotal;
+    calculateTotal();
+    }
+
 
 function calculateTotal() {
     const firstClassCount = getInputValue('firstClass');
@@ -25,12 +28,12 @@ function calculateTotal() {
 
     const totalPrice = firstClassCount * 150 + economyCount * 100;
     document.getElementById('total-price').innerText = totalPrice;
-
+    
     const tax = Math.round(totalPrice * 0.1);
-    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById("tax-amount").innerText = tax;
 
     const grandTotal = totalPrice + tax;
-    document.getElementById('grand-total').innerText = grandTotal;
+    document.getElementById("grand-total").innerText = grandTotal;
 }
 
 function getInputValue(ticket){
